f6fa15985ee618c41db6875f61b7dc8800b622b5
ParasSha24 <168339525+ParasSha24@users.noreply.github.com>
2026-03-23 16:51:01 +1100

    chore: initialize spring boot project structure

---
+++ src/main/java/com/littlepay/transit_calculator/TransitCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.littlepay.transit_calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransitCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransitCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/littlepay/transit_calculator/TransitCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.littlepay.transit_calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransitCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
